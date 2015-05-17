af38c16fb9995c4ec08d9615f815ba41ecaee87a
sunil shrestha <sunil@iwa.fi>
2015-05-17 21:51:55 +0300

    Update deploy script to match host path

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# root@104.131.163.198:/usr/share/nginx/html/bitsocean.com/
+# root@104.131.163.198:/usr/share/nginx/html/seo.bitsocean.com/
 
 server='104.131.163.198'
-public_html='/usr/share/nginx/html/bitsocean.com/public_html'
-new='/usr/share/nginx/html/bitsocean.com/new'
-old='/usr/share/nginx/html/bitsocean.com/old'
+public_html='/usr/share/nginx/html/seo.bitsocean.com/public_html'
+new='/usr/share/nginx/html/seo.bitsocean.com/new'
+old='/usr/share/nginx/html/seo.bitsocean.com/old'
 
 ember build --prod && echo "touch $new && rm -vR $new" | ssh root@$server &&
 scp -r dist root@$server:$new &&
